96a8387fc565d549b6f05e24f3e1a6acb40f5de2
ariasfacundo <facundo.arias@estudiantes.utec.edu.uy>
2021-02-14 21:37:05 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "257ee535-26cd-4c67-af42-51e32644e9d3";
+export const MODAL_CLOSE_SEC = 2.5;
