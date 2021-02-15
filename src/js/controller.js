b2f0cbf10a39d2df5181983548d1bb50648e6066
ariasfacundo <facundo.arias@estudiantes.utec.edu.uy>
2021-02-14 21:45:44 -0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -115,6 +115,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the application!");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -123,6 +127,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchReults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome!");
+  newFeature();
 };
 init();
